22e21e93a1e58870056441414fa71ab57fe9efc8
umairahmed88 <umairahmed_88@ymail.com>
2024-01-08 08:45:13 +0500

    Protected Create User Page and restricted to only admin

--- app/Member/page.jsx
+++ app/Member/page.jsx
@@ -1,6 +1,6 @@
 import { getServerSession } from "next-auth";
 import { options } from "../api/auth/[...nextauth]/options";
-import { redirect } from "next/dist/server/api-utils";
+import { redirect } from "next/navigation";
 
 const Member = async () => {
 	const session = await getServerSession(options);
